85f55fff92a138b58ba59a5e8318be2400ed417b
Sumson90 <Sumkowski.Tomasz@gmail.com>
2023-02-27 23:46:06 +0100

    Create user service

--- src/main/java/pl/animalscharityapp/service/UserService.java
+++ src/main/java/pl/animalscharityapp/service/UserService.java
@@ -1,9 +1,24 @@
 package pl.animalscharityapp.service;
 
 import org.springframework.stereotype.Service;
+import pl.animalscharityapp.model.User;
+import pl.animalscharityapp.reop.UserRepository;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
+    private UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findByUserId(Long Id) {
+        return
+        userRepository.findAllById(Id);
+
+
+    }
 }
